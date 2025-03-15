aab6ffa0f1856626169be0bbdbdd266663264f60
VlasovDmitriy <dimkaaka007@mail.ru>
2025-03-15 10:03:25 +0300

    Start Commit: adding project to the gihub ёпта

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import sliderLogo from './templates/sliderbef.png'
+import factory from './templates/factory.jpg'
+import Logo30years from './templates/logo_30_1.png'
+import './App.css'
+
+export default function App() {
+  
+
+  return (
+    <div>
+      <header>
+        <div className='BigLogo'>
+          <img src={factory} alt="Factory" className='FactoryLogo'/>
+          <img src={sliderLogo} alt="Sliderbef" className='SliderLogo'/>
+          <img src={Logo30years} alt="Logo30years" className='Logo30years' />
+          <h1 className='KNG_Automation'>«Космос-Нефть-Газ Автоматизация»</h1>
+        </div>
+        
+      </header>
+
+    </div>
+     
+  )
+}
+
+
