5dcf82dc1e5a898bae44f9eb1c6a44077fc2ac5a
VlasovDmitriy <dimkaaka007@mail.ru>
2025-03-27 09:11:38 +0300

    branch ready for Romas hands

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -8,18 +8,8 @@ import './Header.css';
 
 export default function Header() {
   return (
-<<<<<<< HEAD
-    <div>
-      <div>
-        <h1 className='proverka'>ПРОВЕРКА ШРИФТА</h1>
-      </div>
-      <div>
-        <h1 className='proverka2'>ПРОВЕРКА ШРИФТА 2</h1>
-      </div>
-      
-=======
-    <>
->>>>>>> e5061ce81ed763bfc4c8ed75c6cb490be0885417
+
+
         <div className='BigLogo'>
           <img src={factory} alt="Factory" className='FactoryLogo' />
           <img src={sliderLogo} alt="Sliderbef" className='SliderLogo' />
@@ -28,27 +18,7 @@ export default function Header() {
           <Phone className='Phone' size={34} strokeWidth={2}/>
           <Mail className='Mail' size={34} strokeWidth={2}/>
         </div>
-<<<<<<< HEAD
 
-        {/* <div className='texts'>
-          <h1 className='lider'>ЛИДЕР В ИТ И АВТОМАТИЗАЦИИ</h1>
-          <h2 className='RV'>РАЗРАБОТКА, ВНЕДРЕНИЕ, АНАЛИЗ, ТЕСТИРОВАНИЕ </h2>
-        </div>
-          
-        <div className='buttons'>
-          <button className='profissionalizm'>Профессионализм</button>
-          <button className='services'>Услуги</button>
-          <button className='products'>Продукты</button>
-        </div> */}
 
-        
-
-        
-    </div>
-
-    
-=======
-    </>
->>>>>>> e5061ce81ed763bfc4c8ed75c6cb490be0885417
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import './index.css';
 import Header from '../Components/Header/Header.jsx'
 
 import Main from '../Components/Main/Main.jsx'
+import Footer from '../Components/Footer/Footer.jsx'
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
+
     </>
   );
 
