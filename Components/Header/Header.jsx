e5061ce81ed763bfc4c8ed75c6cb490be0885417
Reny <reny.zol.2019@gmail.com>
2025-03-27 08:38:35 +0300

    футфетиш с фонтанами

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -8,28 +8,15 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <div>
+    <>
         <div className='BigLogo'>
           <img src={factory} alt="Factory" className='FactoryLogo' />
           <img src={sliderLogo} alt="Sliderbef" className='SliderLogo' />
           <img src={Logo30years} alt="Logo30years" className='Logo30years' />
-          <h1 className='KNG_Automation'>«Космос-Нефть-Газ <br></br> Автоматизация»</h1>
-          <Phone className='Phone' size={34} strokeWidth={3}/>
-          <Mail className='Mail' size={34} strokeWidth={3}/>
+          <h1 className='KNG_Automation'>Космос-Нефть-Газ <br></br> Автоматизация</h1>
+          <Phone className='Phone' size={34} strokeWidth={2}/>
+          <Mail className='Mail' size={34} strokeWidth={2}/>
         </div>
-
-        <div className='texts'>
-          <h1 className='lider'>ЛИДЕР В ИТ И АВТОМАТИЗАЦИИ</h1>
-          <h2 className='RV'>РАЗРАБОТКА, ВНЕДРЕНИЕ, АНАЛИЗ, ТЕСТИРОВАНИЕ </h2>
-        </div>
-          
-        <div className='buttons'>
-          <button className='profissionalizm'>Профессионализм</button>
-          <button className='services'>Услуги</button>
-          <button className='products'>Продукты</button>
-        </div>
-
-        
-    </div>
+    </>
   );
 }
