d404ac71c46de0deb660c35f4aafcdd6f5e4d668
VlasovDmitriy <dimkaaka007@mail.ru>
2025-04-04 15:24:24 +0300

    рома хавай

--- Components/Prof/Prof.jsx
+++ Components/Prof/Prof.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Prof.css';
 
 import Block from './BlockProf.jsx'
-import { Users, Settings  } from 'lucide-react';
+import { Users, Settings, Microchip   } from 'lucide-react';
 export default function(){
 
     return(
@@ -20,7 +20,7 @@ export default function(){
         descriptionText="" 
         />
         <Block 
-        Icon={Users} 
+        Icon={Microchip } 
         text="Технологии и партнеры" 
         descriptionText="Мы используем передовые решения мировых и отечественных лидеров, таких как Siemens, Schneider Electric, 
         Allen Bradley, Emerson, PROSOFT, Trei, Fastwel, Текон, ОВЕН и других. Это позволяет нам предлагать клиентам решения, соответствующие 
