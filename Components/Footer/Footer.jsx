6164921c25ee61e6e41dec4fcf856418f3661fa1
Reny <reny.zol.2019@gmail.com>
2025-04-04 15:22:08 +0300

    кончил от футфетиша

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -1,40 +1,104 @@
 import React from 'react';
 import './Footer.css';
-import { House } from 'lucide-react';
-import Logo30years from "../../templates/logo_30.png";
-import top from "../../templates/footertop.png";
+import { House, Mail, NotebookText, Phone } from 'lucide-react';
+import { CircleArrowUp } from 'lucide-react';
+import Logo30year from "../../templates/logo_30_1.png";
+import BgFooter from "../../templates/bg_footer.png"
 
 export default function Footer() {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Плавная прокрутка
+        });
+    };
     return (
         <div className="footer-wrapper">
-            {/* Контейнер 1: Баннер с фоновым изображением и текстом */}
+            
+            {/* Баннер */}
             <div className="footer-banner-container">
-                <img src={top} alt="Footer background" className='footer-bg' />
                 <div className="footer-content">
                     <h2>ООО «КНГ-Автоматизация»</h2>
                 </div>
             </div>
 
-            {/* Контейнер 2: Информация с адресом и контактами */}
+            {/* Основной контент */}
             <div className="footer-info-container">
                 <div className='container'>
-                    <div className='UridAddress'>Юридический адрес</div>
-                    <div className='Contacts'>Наши контакты</div>
-                    
-                    <div className='address-wrapper'>
-                        <House className='House' size={34} strokeWidth={3} color='red' />
-                        <address className='address'>
-                            <p><strong>Россия, Воронежская область</strong></p>
-                            <p>г. Воронеж</p>
-                            <p>394019, ул. 9 Января, 180 оф. 577</p>
-                        </address>
+                    {/* Юридический адрес */}
+                    <div className='address-block'>
+                        <div className='UridAddress'>Юридический адрес</div>
+                        <div className='address-wrapper'>
+                            <House size={34} strokeWidth={3} color='red' />
+                            <div className='address-text'>
+                                <p><strong>Россия, Воронежская область</strong></p>
+                                <p>г. Воронеж</p>
+                                <p>394019, ул. 9 Января, 180 оф. 577</p>
+                            </div>
+                        </div>
                     </div>
-                    
-                    <div className='logdown'>
-                        <img src={Logo30years} alt="Logo30years" className='Logo30years' />
+
+                    {/* Контакты */}
+                    <div className='contacts-container'>
+                        <div className='ourcontacts'>Наши контакты</div>
+                        
+                        {/* Верхний ряд */}
+                        <div className='contacts-top-row'>
+
+                            {/* Реквизиты */}
+                            <div className='requisites-group'>
+                                <div className='ogrn'>
+                                    <NotebookText size={34} strokeWidth={3} color='red'/>
+                                    <div>
+                                        <p>ОГРН</p>
+                                        <p>1233600001458</p>
+                                    </div>
+                                </div>
+                                <div className='inn'>
+                                    <NotebookText size={34} strokeWidth={3} color='red'/>
+                                    <div>
+                                        <p>ИНН/КПП</p>
+                                        <p>3662303330</p>
+                                        <p>366201001</p>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* Телефон */}
+                            <div className='phone-section'>
+                                <Phone size={34} strokeWidth={3} color='red'/>
+                                <div className='contact-info'>
+                                    <p>Тел. (473) 247-91-00</p>
+                                    <p>Пн - Чт, с 8:00 - 17:00</p>
+                                    <p>Пт 8:00 - 15:45</p>
+                                </div>
+                            </div>
+
+                            
+                        </div>
+
+                        {/* Почта */}
+                        <div className='mail-section'>
+                            <Mail size={34} strokeWidth={3} color='red'/>
+                            <div className='contact-info'>
+                                <p>Пишите нам в любое время!</p>
+                                <p>kng-auto@kng.ru</p>
+                                <p>office@kng.vrn.ru</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+            <img src={BgFooter} alt="Фон" className="footer-bg-image" />
+            <CircleArrowUp 
+                size={34} 
+                strokeWidth={3} 
+                color='red' 
+                className='arrow'
+                onClick={scrollToTop} // Добавьте обработчик клика
+                role="button" // Для доступности
+                aria-label="Прокрутить наверх" // Описание для скринридеров
+            />
+            <img src={Logo30year} alt="30 лет компании" className='Logo30' />
         </div>
     );
 }
